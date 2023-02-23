f1303d1961e93c26da09defb34da95c4d4273b4a
unknown <zaskiakiaa802@gmail.com>
2023-02-23 09:44:32 +0700

    betulin about

--- next-portofolio/pages/About.js
+++ next-portofolio/pages/About.js
@@ -24,34 +24,35 @@ const About = () => {
             src="/aboutme.jpg"
             width={1000}
             height={200}
-            className=' ml-8 w-[400px]  alt="" '
+            className=' ml-8 w-[400px] rounded-xl'
+            alt=""
           />
         </div>
 
-        <div className="flex items-center justify-between w-[300px]">
-          <div className="flex flex-col gap-6">
-            <div className=" text-black flex gap-4 items-center font-medium">
+        <div className="flex items-center justify-between w-[400px]">
+          <div className="flex flex-col gap-6 ">
+            <div className=" text-black flex gap-4 items-center font-medium text-[19px]">
               <BsFillPersonFill className="text-pink-400 text-[24px]" />
               <p>Nama</p>
             </div>
 
-            <div className=" text-black flex gap-4 items-center font-medium">
+            <div className=" text-black flex gap-4 items-center font-medium text-[19px]">
               <FaBirthdayCake className="text-pink-400 text-[24px]" />
               <p>TTL</p>
             </div>
 
-            <div className=" text-black flex gap-4 items-center font-medium">
+            <div className=" text-black flex gap-4 items-center font-medium text-[19px]">
               <FaSchool className="text-pink-400 text-[24px]" />
               <p>Sekolah</p>
             </div>
 
-            <div className=" text-black flex gap-4 items-center font-medium">
+            <div className=" text-black flex gap-4 items-center font-medium text-[19px]">
               <ImLocation2 className="text-pink-400 text-[24px]" />
               <p>Alamat</p>
             </div>
           </div>
 
-          <div className="space-x-5 flex flex-col gap-6 text-black">
+          <div className="space-x-5 flex flex-col gap-6 text-black text-[19px]">
             <div></div>
             <div>:</div>
             <div>:</div>
@@ -60,9 +61,9 @@ const About = () => {
             <div></div>
           </div>
 
-          <div className="space-x-5 flex flex-col gap-6 text-black">
+          <div className="flex flex-col gap-6 text-black text-lg pb-5">
             <div></div>
-            <div> Zaskia Rizky Raichand </div>
+            <div>Zaskia Rizky Raichand</div>
             <div> Kediri, 21 Maret 2006 </div>
             <div> SMK Telkom Malang </div>
             <div> Jl Danau Ranau VII GIF No.03 </div>
@@ -70,9 +71,14 @@ const About = () => {
         </div>
       </div>
       <div className="max-w-[800px] pl-8">
-        <h2 className="font-bold mt-16 mb-3"> Aku Kiaa Yayyyy</h2>
-        <p className=" text-gray-00 mb-7">
+        <h2 className="font-bold mt-16 mb-3 text-[19px]"> Aku Kiaa Yayyyy</h2>
+        <p className=" text-gray-00 text-[15px]">
           Haiii!! Aku Zaskia, biasa dipanggil Kia.
+        </p>
+        <p className=" text-gray-00 text-[15px]">
+          MBTI-ku INFP lohhh wkwkwkwk
+        </p>
+        <p className=" text-gray-00 text-[15px]">
           Aku ingin menjadi Princess Disney
         </p>
 
