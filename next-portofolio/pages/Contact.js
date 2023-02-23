1d1b808b0763d4a75791a0090225eb6c492d3873
unknown <zaskiakiaa802@gmail.com>
2023-02-23 09:07:49 +0700

    contact n creation

--- next-portofolio/pages/Contact.js
+++ next-portofolio/pages/Contact.js
@@ -3,30 +3,68 @@ import React from 'react'
 import Button from '../components/Button'
 import Heading from '../components/Heading'
 import Link from 'next/link'
+import { useState} from 'react'
+import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Contact = () => {
+    const [nama, setNama] = useState('');
+    const [email, setEmail] = useState('');
+    const [subjek, setSubjek] = useState('');
+    const [pesan, setPesan] = useState('');
+
+    const notify = () =>
+    toast.success(' 🦄 Pesan berhasil dikirim!', {
+        position: 'top-center',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'dark',
+    });
+
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+        Nama: nama,
+        Email: email,
+        Subjek: subjek,
+        Pesan: pesan
+    };
+    axios.post('https://sheet.best/api/sheets/d89a032f-0b79-4cf0-81ed-151459a83eb0', data).then((res) => {
+        setNama('');
+        setEmail('');
+        setSubjek('');
+        setPesan('');
+        notify();
+    });
+  };
     return (
     <>
     <Navbar />
     <section className='container mx-auto py-10 px-4' id="contact">
+    <ToastContainer position="top-center" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark" />
         <div className='pl-8'>
         <Heading title="Contact Us" />
         </div>
 
-        <from className='flex flex-col gap-8 pl-8'>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-8 pl-8'>
             <div className='text-sm grid grid-cols-1 md:grid-cols-2 gap-8 '>
-                <input type="text" placeholder='Nama' className=' outline-pink-400 border-gray-400 border rounded-[5px] 4px py-2 w-[100%] pl-3' required />
-                <input type="text" placeholder='Email' className='outline-pink-400 border-gray-400 border rounded-[5px] 4px py-2 w-[100%] pl-3' required />
+                <input type="text" placeholder='Nama' className=' outline-pink-400 border-gray-400 border rounded-[5px] 4px py-2 w-[100%] pl-3'  onChange={(e) => setNama(e.target.value)} value={nama} required />
+                <input type="text" placeholder='Email' className='outline-pink-400 border-gray-400 border rounded-[5px] 4px py-2 w-[100%] pl-3' onChange={(e) => setEmail(e.target.value)} value={email} required />
             </div>
-            <input type="text" placeholder='Subjek' className='outline-pink-400 border-gray-400 border rounded-[5px] 4px py-2 w-[100%] pl-3' required />
+            <input type="text" placeholder='Subjek' className='outline-pink-400 border-gray-400 border rounded-[5px] 4px py-2 w-[100%] pl-3' onChange={(e) => setSubjek(e.target.value)} value={subjek} required />
 
-            <textarea rows="6" placeholder='Pesan' className='outline-pink-400 border-gray-400 border rounded-[5px] 4px py-2 w-[100%] pl-3' required>
+            <textarea rows="6" placeholder='Pesan' className='outline-pink-400 border-gray-400 border rounded-[5px] 4px py-2 w-[100%] pl-3' onChange={(e) => setPesan(e.target.value)} value={pesan} required>
 
             </textarea>
             <div>
-                <Button link='About' text='Kirim Pesan' />
+                <button type='submit' className='btn bg-pink-400 px-8 py-3 rounded-lg'>Kirim</button>
             </div>
-        </from>
+        </form>
     </section>
     </>
     )
--- next-portofolio/pages/Creation.js
+++ next-portofolio/pages/Creation.js
@@ -14,29 +14,69 @@ const Creation = () => {
 
         <div className='grid grid-cols-3 gap-4 ml-8'>
             <div className='row-span-2 relative group'>
-                <Image src="/oc full.jpeg" width="1000" height="100" alt="" />
-                <div className='bg-black absolute w-[100%] top-0
+                <Image src="/oc full.jpeg" width="1000" height="100" alt="" className='border-4 border-zinc-200' /> 
+            </div>
+
+            <div className='relative group'>
+                <Image src="/oc half happy.jpeg" width="1000" height="100" alt="" className='border-4 border-zinc-200' />
+                <div className='bg-black absolute w-[100%] h-[100%] top-0
                 opacity-0 transition duration-500 group:hover:opacity-100
                 grid place-items-center text-white'>
-                    Project Title
+                    Half Body Happy
                 </div>
             </div>
 
             <div className='relative group'>
-                <Image src="/oc half happy.jpeg" width="1000" height="100" alt="" />
+                <Image src="/oc half sad.jpeg" width="1000" height="100" alt="" className='border-4 border-zinc-200' />
                 <div className='bg-black absolute w-[100%] h-[100%] top-0
                 opacity-0 transition duration-500 group:hover:opacity-100
                 grid place-items-center text-white'>
-                    Project Title
+                    Half Body Cry
                 </div>
             </div>
 
             <div className='relative group'>
-                <Image src="/oc half sad.jpeg" width="1000" height="100" alt="" />
+                <Image src="/ayam.png" width="1000" height="100" alt="" className='border-4 border-zinc-200' />
                 <div className='bg-black absolute w-[100%] h-[100%] top-0
                 opacity-0 transition duration-500 group:hover:opacity-100
                 grid place-items-center text-white'>
-                    Project Title
+                    Blender Ayam
+                </div>
+            </div>
+
+            <div className='relative group'>
+                <Image src="/ayo masak.png" width="1000" height="100" alt="" className='border-4 border-zinc-300' />
+                <div className='bg-black absolute w-[100%] h-[100%] top-0
+                opacity-0 transition duration-500 group:hover:opacity-100
+                grid place-items-center text-white'>
+                    Ayo masak
+                </div>
+            </div>
+
+            <div className='relative group'>
+                <Image src="/padoru.jpeg" width="1000" height="100" alt="" className='border-4 border-zinc-200' />
+                <div className='bg-black absolute w-[100%] h-[100%] top-0
+                opacity-0 transition duration-500 group:hover:opacity-100
+                grid place-items-center text-white'>
+                    Chibi Padoru
+                </div>
+            </div>
+
+            <div className='relative group'>
+                <Image src="/project p5.jpeg" width="1000" height="100" alt="" className='border-4 border-zinc-200' />
+                <div className='bg-black absolute w-[100%] h-[100%] top-0
+                opacity-0 transition duration-500 group:hover:opacity-100
+                grid place-items-center text-white'>
+                    Project P5
+                </div>
+            </div>
+
+            <div className='relative group'>
+                <Image src="/oc sun flower.jpeg" width="1000" height="100" alt="" className='border-4 border-zinc-200' />
+                <div className='bg-black absolute w-[100%] h-[100%] top-0
+                opacity-0 transition duration-500 group:hover:opacity-100
+                grid place-items-center text-white'>
+                    Original Character
                 </div>
             </div>
 
